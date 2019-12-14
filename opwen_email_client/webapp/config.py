290b417c4965fdb5dd553b18bee9ac0fe2143b04
sbathgate <sbathgate03@gmail.com>
2019-12-14 14:15:16 -0800

    Issue 157 - Added resizing of images attached to emails. Adjusted config file to meet PEP8 Guidelines

--- opwen_email_client/webapp/config.py
+++ opwen_email_client/webapp/config.py
@@ -14,9 +14,11 @@ env.read_env(settings_path, recurse=False)
 app_basedir = path.abspath(path.dirname(__file__))
 root_domain = env('OPWEN_ROOT_DOMAIN', 'lokole.ca')
 
+
 class ImageDimensions(object):
     MAX_WIDTH_IMAGES = env.int('LOKOLE_MAX_WIDTH_EMAIL_IMAGES', 200)
     MAX_HEIGHT_IMAGES = env.int('LOKOLE_MAX_HEIGHT_EMAIL_IMAGES', 200)
+
 
 # noinspection PyPep8Naming
 class i8n(object):
